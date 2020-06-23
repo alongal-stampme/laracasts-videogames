1cb8a10acbe0d86dfba540e52ae86c8b1e5470e8
Alon Gal <alon@stampme.com>
2020-06-23 21:54:04 +1000

    installed tailwind and started the project

---
+++ src/laracasts-videogames/resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracasts Video Games</title>
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
