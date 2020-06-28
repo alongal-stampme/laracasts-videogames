0f36aaad71573dd8421a7cbe554aad6f5cad8bc6
Alon Gal <alon@stampme.com>
2020-06-28 13:21:37 +1000

    Style a Single Game Page with Tailwind

--- src/laracasts-videogames/resources/views/index.blade.php
+++ src/laracasts-videogames/resources/views/index.blade.php
@@ -16,7 +16,6 @@
                 <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy 7 Remake</a>
                 <div class="text-gray-400 mt-1">Playstation 4</div>
             </div>
-
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
