8fe3f271a02a326951ce1cc3446c59f5ca0915fc
Alon Gal <alon@stampme.com>
2020-07-04 18:29:21 +1000

    Skeleton Loader

--- src/laracasts-videogames/resources/views/livewire/popular-games.blade.php
+++ src/laracasts-videogames/resources/views/livewire/popular-games.blade.php
@@ -23,8 +23,14 @@
             </div>
         </div>
     @empty
-        <div>
-            <div class="spinner mt-8"></div>
-        </div>
+        @foreach (range(1, 12) as $game)
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <div class="bg-gray-800 w-44 h-56"></div>
+                </div>
+                <div class="block text-transparent text-lg bg-gray-700 rounded leading-tight mt-4 ">Title goes here</div>
+                <div class="text-transparent bg-gray-700 rounded mt-3 inline-block">PS4, PC, Switch </div>
+            </div>
+        @endforeach
     @endforelse
 </div> <!-- end popular-games -->
