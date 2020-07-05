b713e62e27ee99658fb0fca52a376f5868b6f895
Alon Gal <alon@stampme.com>
2020-07-05 12:12:12 +1000

    Use the API to Fetch Details for a Single Game

--- src/laracasts-videogames/app/Http/Controllers/GamesController.php
+++ src/laracasts-videogames/app/Http/Controllers/GamesController.php
@@ -64,11 +64,6 @@ class GamesController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
@@ -85,15 +80,31 @@ class GamesController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $game = Http::withHeaders(config('services.igdb'))
+            ->withOptions([
+                'body' => "
+                    fields name, cover.url, first_release_date, popularity,
+                    platforms.abbreviation, rating, slug,
+                    involved_companies.company.name, genres.name,
+                    aggregated_rating, summary, websites.*, videos.*,
+                    screenshots.*, similar_games.cover.url,
+                    similar_games.name, similar_games.rating,
+                    similar_games.platforms.abbreviation, similar_games.slug;
+                    where slug=\"{$slug}\";
+                "
+            ])
+            ->get('https://api-v3.igdb.com/games/')
+            ->json();
+
+        dump($game);
+
+        abort_if(! $game, 404);
+
+        return view('show', [
+            'game' => $game[0],
+        ]);
     }
 
     /**
--- src/laracasts-videogames/routes/web.php
+++ src/laracasts-videogames/routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'GamesController@index')->name('games.index');
+Route::get('/games/{slug}', 'GamesController@show')->name('games.show');
 
 //Route::get('/', function () {
 //    return view('index');
